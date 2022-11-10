c43726eaba5752cb481c86d9c9b8330fbed02d41
Michal Bernát <113421230+michau6666@users.noreply.github.com>
2022-11-10 08:39:11 +0100

    Update Numbers.cs

--- Open-Lab-04.06/Numbers.cs
+++ Open-Lab-04.06/Numbers.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_04._06
 {
@@ -6,7 +6,23 @@ namespace Open_Lab_04._06
     {
         public int[] NoOdds(int[] numbers)
         {
-            throw new NotImplementedException();
+            int num = 0;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                    num++;
+            }
+            int[] pidar = new int[num];
+            int had = 0;
+            for(int j = 0; j < numbers.Length; j++)
+            {
+                if(numbers[j] % 2 == 0)
+                {
+                    pidar[had] = numbers[j];
+                    had++;
+                }
+            }
+            return pidar;
         }
     }
 }
